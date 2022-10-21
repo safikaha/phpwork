038864bc6c4573ff0446d237806729136522dcf3
safikaha <s64309010003@ptnicec.ac.th>
2022-10-21 11:50:54 +0700

    ส่งไฟล์ comment

---
+++ comment.php
@@ -0,0 +1,15 @@
+<?php
+
+    //บรรทัดที่ 6-7 แสดงผลการใช้งาน Cmment แบบบรรทัดเดียว 
+  echo "Shafika Ha<br>";
+  echo "Information Technology";
+  //ecch "Pattani<br>";
+  #ecch "Thailand";
+
+  //บรรทัดที่ 10-12 แสดงผลการใช้งาน Cmment แบบบรรทัดเดียว 
+  /*echo "<br>";
+  echo "94000<br>";
+  echo "Asia<br>"; */
+  echo "Universal<br>";
+
+?>
